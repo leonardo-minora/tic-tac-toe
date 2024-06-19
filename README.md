b4c042de7cb71a6bf17728aaf25519d847c5a0d8
Leonardo Minora <leonardo.minora@gmail.com>
2024-06-18 21:09:52 -0300

    Momento 2 - Até renderização do estado

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ O projeto React foi criado usando o [vite](https://vitejs.dev/) usando o templat
 - Para instalar as bibliotecas, execute `npm install`
 - Para executar o projeto, execute `npm run dev`
 - Lembre de abrir o navegador e acessar o endereço http://localhost:5173/
+
+
+## Momento 2
+1. Unificando em apenas um evento para escutar os clicks dos componentes `Square`
+2. Alternando entre os jogadores X e O
+3. Verficando se o jogador já clicou no `Square`
+4. Verficando se existe ganhador
+5. Apresentando estado do jogo
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import { useState } from "react";
 
-// import 'main.css'
-
 const Square = (props: any) => {
-	const [valor, setValor] = useState(props.value);
-	const escutarClique = () => {
-		console.log("clicou no botão");
-		setValor("X");
-	};
-
-	const escutar = props.onClique ?? escutarClique;
+	const escutar = props.onClique ?? null;
 	const titulo = props.value ?? "NOK";
 
 	return (
@@ -20,74 +12,130 @@ const Square = (props: any) => {
 };
 
 const Board = () => {
-	const [botao_1_1, setModificarValorBotao_1_1] = useState("1");
-	const [botao_1_2, setModificarValorBotao_1_2] = useState("2");
-	const [botao_1_3, setModificarValorBotao_1_3] = useState("3");
-	const [botao_2_1, setModificarValorBotao_2_1] = useState("4");
-	const [botao_2_2, setModificarValorBotao_2_2] = useState("5");
-	const [botao_2_3, setModificarValorBotao_2_3] = useState("6");
-	const [botao_3_1, setModificarValorBotao_3_1] = useState("7");
-	const [botao_3_2, setModificarValorBotao_3_2] = useState("8");
-	const [botao_3_3, setModificarValorBotao_3_3] = useState("9");
+	const [nextGamer, setNextGamer] = useState("X");
+	const [botao_1_1, setModificarValorBotao_1_1] = useState("");
+	const [botao_1_2, setModificarValorBotao_1_2] = useState("");
+	const [botao_1_3, setModificarValorBotao_1_3] = useState("");
+	const [botao_2_1, setModificarValorBotao_2_1] = useState("");
+	const [botao_2_2, setModificarValorBotao_2_2] = useState("");
+	const [botao_2_3, setModificarValorBotao_2_3] = useState("");
+	const [botao_3_1, setModificarValorBotao_3_1] = useState("");
+	const [botao_3_2, setModificarValorBotao_3_2] = useState("");
+	const [botao_3_3, setModificarValorBotao_3_3] = useState("");
 
-	const escutarCliqueBotao_1_1 = () => {
-		setModificarValorBotao_1_1("X");
+	const escutarClique = (indice: number) => {
+		const vencedor = calcularVencedor();
+		console.info(vencedor);
+		if (vencedor !== "") return;
+
+		if (indice === 0) {
+			if (botao_1_1 !== "") return;
+			setModificarValorBotao_1_1(nextGamer);
+		}
+		else if (indice === 1) {
+			if (botao_1_2 !== "") return;
+			setModificarValorBotao_1_2(nextGamer);
+		}
+		else if (indice === 2) {
+			if (botao_1_3 !== "") return;
+			setModificarValorBotao_1_3(nextGamer);
+		}
+		else if (indice === 3) {
+			if (botao_2_1 !== "") return;
+			setModificarValorBotao_2_1(nextGamer);
+		}
+		else if (indice === 4) {
+			if (botao_2_2 !== "") return;
+			setModificarValorBotao_2_2(nextGamer);
+		}
+		else if (indice === 5) {
+			if (botao_2_3 !== "") return;
+			setModificarValorBotao_2_3(nextGamer);
+		}
+		else if (indice === 6) {
+			if (botao_3_1 !== "") return;
+			setModificarValorBotao_3_1(nextGamer);
+		}
+		else if (indice === 7) {
+			if (botao_3_2 !== "") return;
+			setModificarValorBotao_3_2(nextGamer);
+		}
+		else if (indice === 8) {
+			if (botao_3_3 !== "") return;
+			setModificarValorBotao_3_3(nextGamer);
+		}
+		
+		const gamer = nextGamer === "X" ? "O" : "X";
+		setNextGamer(gamer);
 	};
 
-	const escutarCliqueBotao_1_2 = () => {
-		setModificarValorBotao_1_2("X");
-	};
+	const calcularVencedor = () => {
+		/*
+		[0, 1, 2]
+		[3, 4, 5],
+    	[6, 7, 8],
+    	[0, 3, 6],
+    	[1, 4, 7],
+    	[2, 5, 8],
+    	[0, 4, 8],
+    	[2, 4, 6]
+		*/
+		// linhas
+		if (botao_1_1 && botao_1_1 === botao_1_2 && botao_1_1 === botao_1_3) {
+			return botao_1_1;
+		} 
+		else if (botao_2_1 && botao_2_1 === botao_2_2 && botao_2_1 === botao_2_3) {
+			return botao_2_1;
+		}
+		else if (botao_3_1 && botao_3_1 === botao_3_2 && botao_3_1 === botao_3_3) {
+			return botao_3_1;
+		}
+		// colunas
+		else if (botao_1_1 && botao_1_1 === botao_2_1 && botao_1_1 === botao_3_1) {
+			return botao_1_1;
+		}
+		else if (botao_1_2 && botao_1_2 === botao_2_2 && botao_1_2 === botao_3_2) {
+			return botao_1_2;
+		}
+		else if (botao_1_3 && botao_1_3 === botao_2_3 && botao_1_3 === botao_3_3) {
+			return botao_1_3;
+		}
+		// diagonais
+		else if (botao_1_1 && botao_1_1 === botao_2_2 && botao_1_1 === botao_3_3) {
+			return botao_1_1;
+		}
+		else if (botao_1_3 && botao_1_3 === botao_2_2 && botao_1_3 === botao_3_1) {
+			return botao_1_3;
+		}
 
-	const escutarCliqueBotao_1_3 = () => {
-		setModificarValorBotao_1_3("X");
-	};
+		return "";
+	}
+
+	const vencedor = calcularVencedor();
+	let estado;
+	if (vencedor !== "") {
+		estado = `Vencedor ${vencedor}`;
+	} else {
+		estado = `Próximo jogador: ${nextGamer}`;
+	}
 
 	return (
 		<>
+			<div className="status">{estado}</div>
 			<div className="board-row">
-				<Square value={botao_1_1} onClique={escutarCliqueBotao_1_1} />
-				<Square value={botao_1_2} onClique={escutarCliqueBotao_1_2} />
-				<Square value={botao_1_3} onClique={escutarCliqueBotao_1_3} />
+				<Square value={botao_1_1} onClique={() => (vencedor === "") && escutarClique(0)} />
+				<Square value={botao_1_2} onClique={() => escutarClique(1)} />
+				<Square value={botao_1_3} onClique={() => escutarClique(2)} />
 			</div>
 			<div className="board-row">
-				<Square
-					value={botao_2_1}
-					onClique={() => {
-						setModificarValorBotao_2_1("X");
-					}}
-				/>
-				<Square
-					value={botao_2_2}
-					onClique={() => {
-						setModificarValorBotao_2_2("X");
-					}}
-				/>
-				<Square
-					value={botao_2_3}
-					onClique={() => {
-						setModificarValorBotao_2_3("X");
-					}}
-				/>
+				<Square value={botao_2_1} onClique={() => escutarClique(3)} />
+				<Square value={botao_2_2} onClique={() => escutarClique(4)} />
+				<Square value={botao_2_3} onClique={() => escutarClique(5)} />
 			</div>
 			<div className="board-row">
-				<Square
-					value={botao_3_1}
-					onClique={() => {
-						setModificarValorBotao_3_1("X");
-					}}
-				/>
-				<Square
-					value={botao_3_2}
-					onClique={() => {
-						setModificarValorBotao_3_2("X");
-					}}
-				/>
-				<Square
-					value={botao_3_3}
-					onClique={() => {
-						setModificarValorBotao_3_3("X");
-					}}
-				/>
+				<Square value={botao_3_1} onClique={() => escutarClique(6)} />
+				<Square value={botao_3_2} onClique={() => escutarClique(7)} />
+				<Square value={botao_3_3} onClique={() => escutarClique(8)} />
 			</div>
 		</>
 	);
